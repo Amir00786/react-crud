3a125607014c6f8fc094148aff5e795036e28823
Amir Shahzad <amirshahzadvu91@gmail.com>
2025-09-08 10:53:57 +0500

    feat: update api

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import ProductPage from "./productpage";
 function App() {
   return (
     <>
-      <div className="max-w-5xl mx-auto py-10">
+      <div className="max-w-5xl mx-auto py-8">
         <ProductPage />
         {/* <ProductsEra /> */}
       </div>
